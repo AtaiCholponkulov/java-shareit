5d978e92e41ef1d7c16a8d46a04d8a05c9fc4889
Atai_Cholponkulov <ataicholponkulov@gmail.com>
2023-06-10 01:43:48 +0600

    Sprint 14 - v1.0 nativeQuery = false for item search by word

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -10,10 +10,8 @@ public interface ItemRepository extends JpaRepository<Item, Integer> {
 
     List<Item> findAllByOwnerIdOrderById(Integer ownerId);
 
-    @Query(nativeQuery = true,
-            value = "SELECT * " +
-                    "FROM items AS i " +
-                    "WHERE i.is_available = true " +
-                    "AND (LOWER(i.name) LIKE '%?1%' OR LOWER(i.description) LIKE '%?1%')")//db COLLATE and CTYPE settings should be specific to support cyrillic
+    @Query("SELECT i " +
+            "FROM Item i " +
+            "WHERE i.available = true AND LOWER(i.description) LIKE %?1%")//db COLLATE and CTYPE settings should be specific to support cyrillic
     List<Item> findAllAvailableItemsByWord(String word);
 }
