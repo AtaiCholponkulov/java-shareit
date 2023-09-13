d5fc3071db1de869b9b00aee8704ff69888a10c0
Atai_Cholponkulov <ataicholponkulov@gmail.com>
2023-09-14 04:21:06 +0600

    Sprint 16 - first try

--- gateway/src/main/java/ru/practicum/shareit/exception/ExceptionResponse.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/ExceptionResponse.java
@@ -1,13 +1,12 @@
 package ru.practicum.shareit.exception;
 
+import lombok.Getter;
+
+@Getter
 public class ExceptionResponse {
     private final String error;
 
     public ExceptionResponse(String error) {
         this.error = error;
-    }
-
-    public String getError() {
-        return error;
     }
 }
